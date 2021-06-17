800e94f5be58d934d6b75a3a022291a1cb22dcc5
yuhongyang <yuhongyang@juwang.cn>
2021-06-17 11:54:49 +0800

    fix：删除多余配置git

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
-import _ from 'lodash'
+import _ from 'lodash';
+import Print from './print';
+
 function component() {
-  // const element = document.createElement('div');
-
-  // // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  // element.classList.add('hello');
-  // // 将图像添加到我们已经存在的 div 中。
-  // const myIcon = new Image();
-  // myIcon.src = Icon;
-
-  // element.appendChild(myIcon);
-  // return element;
-
   const element = document.createElement('div');
-  const btn = document.createElement('button');
 
+  // lodash 是由当前 script 脚本 import 进来的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.onclick = Print.bind(null, 'Hello webpack!');
 
   return element;
 }
